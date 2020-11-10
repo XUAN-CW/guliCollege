1dcc9b5a27d062adf559b7f017e5ecb425f8db66
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-10 23:49:24 +0800

    MybatisPlus 实现复杂查询操作

--- code/mpdemo1010/src/test/java/com/atguigu/mpdemo1010/Mpdemo1010ApplicationTests.java
+++ code/mpdemo1010/src/test/java/com/atguigu/mpdemo1010/Mpdemo1010ApplicationTests.java
@@ -153,4 +153,40 @@ public class Mpdemo1010ApplicationTests {
         System.out.println(result);
     }
 
+    //mp实现复杂查询操作
+    @Test
+    public void testSelectQuery() {
+        //创建QueryWrapper对象
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+
+        //通过QueryWrapper设置条件
+        //ge、gt、le、lt
+        //查询age>=30记录
+        //第一个参数字段名称，第二个参数设置值
+//        wrapper.ge("age",30);
+
+        //eq、ne
+        //wrapper.eq("name","lilei");
+        //wrapper.ne("name","lilei");
+
+        //between
+        //查询年龄 20-30
+        // wrapper.between("age",20,30);
+
+        //like
+        //wrapper.like("name","l");
+
+        //orderByDesc
+        // wrapper.orderByDesc("id");
+
+        //last
+        //wrapper.last("limit 1");
+
+        //指定要查询的列
+        wrapper.select("id","name");
+
+        List<User> users = userMapper.selectList(wrapper);
+        System.out.println(users);
+
+    }
 }
