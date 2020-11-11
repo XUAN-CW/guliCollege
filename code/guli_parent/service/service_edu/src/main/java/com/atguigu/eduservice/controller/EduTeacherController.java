0220ef35fb46778dd0da794a67e662df4469dd3f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-11 20:19:30 +0800

    统一返回数据格式

--- code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.atguigu.commonutils.R;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,5 +42,29 @@ public class EduTeacherController {
         return teacherService.removeById(id);
     }
 
+
+    //1 查询讲师表所有数据
+    //http://localhost:8001/eduservice/teacher/R_findAll
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("R_findAll")
+    public R R_findAllTeacher() {
+        //调用service的方法实现查询所有的操作
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
+    }
+
+    //2 逻辑删除讲师的方法
+    //http://localhost:8001/eduservice/teacher/R/1
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("R/{id}")
+    public R R_removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
+                           @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if(flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
 }
 
