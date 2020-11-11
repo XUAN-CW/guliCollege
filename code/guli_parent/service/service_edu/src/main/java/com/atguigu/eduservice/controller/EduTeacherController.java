dd57720b87c409d6816b987b80143d6af5f869f0
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-11 14:21:19 +0800

    查询讲师表所有数据
    application.properties 添加
    
    ```
    spring.jackson.date-format=yyyy-MM-dd HH:mm:ss
    spring.jackson.time-zone=GMT+8
    ```
    
    否则报错

--- code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,10 +1,15 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 /**
  * <p>
  * 讲师 前端控制器
@@ -16,6 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1、查询讲师表所有数据
+    //http://localhost:8001/eduservice/teacher/findAll
+    @GetMapping("findAll")
+    public List<EduTeacher> findAllTeacher() {
+        //调用service的方法实现查询所有的操作
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
+
 
 }
 
