95f3b76af8086720d8bb2c012716786173da618b
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-15 11:15:31 +0800

    跟视频保持一致

--- code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ code/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-15
  */
 @RestController
-@RequestMapping("/eduservice/edu-subject")
+@RequestMapping("/eduservice/subject")
 public class EduSubjectController {
 
 }
