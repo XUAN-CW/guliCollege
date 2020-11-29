5cab4e0c12615c6fda824b8b24cc510352c00081
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-30 00:03:47 +0800

    复制了 day14 代码

--- startFromDay05/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
+++ startFromDay05/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -1,11 +1,15 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.frontvo.CourseFrontVo;
+import com.atguigu.eduservice.entity.frontvo.CourseWebVo;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
 import com.atguigu.eduservice.entity.vo.CoursePublishVo;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -31,4 +35,10 @@ public interface EduCourseService extends IService<EduCourse> {
 
     //删除课程
     void removeCourse(String courseId);
+
+    //1 条件查询带分页查询课程前台
+    Map<String, Object> getCourseFrontList(Page<EduCourse> pageCourse, CourseFrontVo courseFrontVo);
+
+    //根据课程id，编写sql语句查询课程信息
+    CourseWebVo getBaseCourseInfo(String courseId);
 }
